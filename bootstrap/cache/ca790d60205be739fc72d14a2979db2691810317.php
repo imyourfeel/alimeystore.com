26303cd12697a6573ab85e7bcf2afda47fd94f88
Rithya Theng <imyourfeel@gmail.com>
2021-05-26 18:58:10 +0700

    update admin
    update admin panel

--- bootstrap/cache/ca790d60205be739fc72d14a2979db2691810317.php
+++ bootstrap/cache/ca790d60205be739fc72d14a2979db2691810317.php
@@ -4,23 +4,23 @@
     <h3> Welcome Admin </h3>
     
     <div class="image-holder text-center">
-        <?php if(user()->fullname='Rithya Theng'): ?>
-            <img src="/images/rithya2.jpg" alt="rithya" title="Admin">}
+        <?php if(user()->fullname=='Lek Piseth'): ?>
+            <img src="/images/piseth2.jpg" alt="rithya" title="Admin">
         <?php else: ?>
-            <img src="/images/piseth2.jpg" alt="piseth" title="Admin">}
+            <img src="/images/rithya2.jpg" alt="piseth" title="Admin">
         <?php endif; ?>
         <p><?php echo e(user()->fullname); ?></p>
     </div>
     
        <ul class="vertical menu">
-        <li><a href="/admin"><i class="fa fa-area-chart fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a></li>
-        <li><a href="/admin/product/create"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Product</a></li>
-        <li><a href="/admin/products"><i class="fa fa-tasks fa-fw" aria-hidden="true"></i>&nbsp; Manage Products</a></li>
-        <li><a href="/admin/product/categories"><i class="fa fa-folder-open fa-fw" aria-hidden="true"></i> &nbsp;Categories</a></li>
-        <li><a href="/admin/transactions/orders"><i class="fa fa-cart-arrow-down fa-fw" aria-hidden="true"></i>&nbsp;&nbsp;Orders</a></li>
-        <li><a href="/admin/transactions/payments"><i class="fa fa-dollar fa-fw" aria-hidden="true"></i>&nbsp; Payments</a></li>
-        <li><a href="/logout"><i class="fa fa-sign-out fa-fw" aria-hidden="true"></i>&nbsp; Logout</a></li>
-    </ul>
+            <li><a href="/admin"><i class="fa fa-area-chart fa-fw" aria-hidden="true"></i>&nbsp; Dashboard</a></li>
+            <li><a href="/admin/product/create"><i class="fa fa-plus fa-fw" aria-hidden="true"></i>&nbsp; Add Product</a></li>
+            <li><a href="/admin/products"><i class="fa fa-tasks fa-fw" aria-hidden="true"></i>&nbsp; Manage Products</a></li>
+            <li><a href="/admin/product/categories"><i class="fa fa-folder-open fa-fw" aria-hidden="true"></i> &nbsp;Categories</a></li>
+            <li><a href="/admin/transactions/orders"><i class="fa fa-cart-arrow-down fa-fw" aria-hidden="true"></i>&nbsp;&nbsp;Orders</a></li>
+            <li><a href="/admin/transactions/payments"><i class="fa fa-dollar fa-fw" aria-hidden="true"></i>&nbsp; Payments</a></li>
+            <li><a href="/logout"><i class="fa fa-sign-out fa-fw" aria-hidden="true"></i>&nbsp; Logout</a></li>
+        </ul>
    
 </div>
 <!-- end Side bar -->
