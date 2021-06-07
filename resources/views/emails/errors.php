d3c8f5b479a90d40fe65ccfba1f29a41211868c3
Rithya Theng <imyourfeel@gmail.com>
2021-06-07 17:46:35 +0700

    Optimized

--- resources/views/emails/errors.php
+++ resources/views/emails/errors.php
@@ -7,11 +7,13 @@
 </head>
 <body>
 <div style="width: 600px; padding: 15px; margin: 0 auto; background-color: #0a0a0a; color: #ffffff;">
-    <img src="https://dbqq2ditazz67.cloudfront.net/logos/sm-white-yellow.png" width="150" height="187" />
+    <<div style="text-align: center; width: 200px; margin:  0 auto;">
+            <img style="height: 50px; width: 50px;" src="https://i.ibb.co/q7t57dJ/alimeystore-icon.jpg" alt="alimeystore-icon" border="0">
+        </div>
      <?php echo "Error: {$data}" ?>
     <p>
         Regards <br /> <br />
-        <strong>Devscreencast Team</strong>
+        <strong>Alimeystore</strong>
     </p>
 </div>
 </body>
--- resources/views/emails/welcome.php
+++ resources/views/emails/welcome.php
@@ -6,7 +6,9 @@
 </head>
 <body>
     <div style="width: 600px; padding: 15px; margin: 0 auto; background-color: #0a0a0a;">
-        <img src="https://i.ibb.co/q7t57dJ/alimeystore-icon.jpg" alt="alimeystore-icon" border="0" width="150" height="187" />
+       <div style="text-align: center; width: 200px; margin:  0 auto;">
+            <img style="height: 50px; width: 50px;" src="https://i.ibb.co/q7t57dJ/alimeystore-icon.jpg" alt="alimeystore-icon" border="0">
+        </div>
         email body <?php echo $data ?>
 
        <p>
